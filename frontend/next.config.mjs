2858c53f6cfa8d77c3aad67c4f679e4c652b4bea
Joel Sajan <joel.ukrasajan@ucdconnect.ie>
2025-01-03 03:28:11 +0000

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-      ACCOUNTS_SERVICE_URL: "http://accounts:8081",
-      CARDS_SERVICE_URL: "http://cards:8090",
-      LOANS_SERVICE_URL: "http://loans:9000",
+      ACCOUNTS_SERVICE_URL: "http://localhost:8081",
+      CARDS_SERVICE_URL: "http://localhost:8090",
+      LOANS_SERVICE_URL: "http://localhost:9000",
     },
   };
   
